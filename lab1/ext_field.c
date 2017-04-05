63d215e0b05752fec5eef320b3b5d94b5b92f492
Declan Kelly <dkelly.home@gmail.com>
2017-04-05 11:46:16 -0700

    Finished exercise 6, added make demo

--- lab1/ext_field.c
+++ lab1/ext_field.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <limits.h>
 
-int detect_carry(unsigned char a, unsigned char b)
+int detect_carry(unsigned char a, unsigned char b, unsigned char c)
 {
-    unsigned char result = a + b;
-
-    if (result < a + b)
+    unsigned char result = a + b + c;
+    
+    if (result < a + b + c)
     {
-        return (a + b) - result;
+        return (a + b + c) - UCHAR_MAX;
     }
     else
     {
@@ -23,10 +25,49 @@ unsigned int arbitrary_byte_add(
 {
     int idx;
     unsigned int carry = carry_in;
-    for (idx = 0; i < size; idx++)
+    for (idx = size - 1; idx >= 0; idx--)
     {
-        
+        result[idx] = (unsigned char) (a1[idx] + a2[idx] + carry);
+        carry = detect_carry(a1[idx], a2[idx], carry);
     }
+
+    return carry;
+}
+
+void output_add(
+    unsigned char *result,
+    unsigned char *a1,
+    unsigned char *a2,
+    int size)
+{
+    int idx;
+    unsigned int carry = 0;
+
+    memset(result, 0, size * sizeof(unsigned char));
+
+    for (idx = 0; idx < size; idx++)
+    {
+        printf("\t0x%x", a1[idx]);
+    }
+    printf("\n");
+
+    printf("+");
+    for (idx = 0; idx < size; idx++)
+    {
+        printf("\t0x%x", a2[idx]);
+    }
+    printf("\n");
+
+    carry = arbitrary_byte_add(result, a1, a2, size, carry);
+
+    printf("=");
+    for (idx = 0; idx < size; idx++)
+    {
+        printf("\t0x%x", result[idx]);
+    }
+    printf("\n\n");
+
+    printf("Carry Out = %d\n", carry);
 }
 
 int main(int argc, char const *argv[])
@@ -37,17 +78,26 @@ int main(int argc, char const *argv[])
     unsigned char test1b[] = {
         0x30, 0xFF, 0xFF, 0x00, 0x0
     };
-
     unsigned char test2a[] = {
         0xFF, 0xFF, 0xFF, 0xFF, 0xFF
     };
     unsigned char test2b[] = {
         0x00, 0x00, 0x00, 0x00, 0x01
     };
-    unsigned int carry = 0;
+    unsigned char result[5];
     int size = 5;
 
-    unsigned char result[5];
+    printf("Part 6: Performing Extended Field Arithmetic\n");
+    printf("========================\n");
+
+    printf("6a)\n");
+    output_add(result, test1a, test1b, size);
+
+    printf("6b)\n");
+    output_add(result, test2a, test2b, size);
+
+
+    printf("========================\n");
 
     return 0;
 }
--- lab1/sizes.c
+++ lab1/sizes.c
@@ -4,10 +4,10 @@ int main(int argc, char const *argv[])
 {
     printf("Part 1: Data Types and their Sizes\n");
     printf("========================\n");
-    printf("Byte Size: %d\n", sizeof(char));
-    printf("Short Int Size: %d\n", sizeof(short));
-    printf("Integer Size: %d\n", sizeof(int));
-    printf("Long Integer Size: %d\n", sizeof(long));
+    printf("Byte Size: %lu\n", sizeof(char));
+    printf("Short Int Size: %lu\n", sizeof(short));
+    printf("Integer Size: %lu\n", sizeof(int));
+    printf("Long Integer Size: %lu\n", sizeof(long));
     printf("========================\n");
 
     return 0;
